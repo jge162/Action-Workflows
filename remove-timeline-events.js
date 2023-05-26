b2d825ae06eedc02b7e8025490591d8c0b65e2bd
Jermyiah <escobarjeremy@csu.fullerton.edu>
2023-05-26 11:58:31 -0700

    Create remove-timeline-events.js

---
+++ remove-timeline-events.js
@@ -0,0 +1,37 @@
+const axios = require('axios');
+
+async function removeEvents() {
+  const authToken = 'YOUR_PERSONAL_ACCESS_TOKEN';
+  const prNumber = process.env.GITHUB_EVENT_NAME === 'pull_request' ? process.env.GITHUB_EVENT_NUMBER : null;
+  const owner = process.env.GITHUB_REPOSITORY_OWNER;
+  const repo = process.env.GITHUB_REPOSITORY;
+
+  try {
+    if (prNumber) {
+      // Fetch comments for the pull request
+      const response = await axios.get(`https://api.github.com/repos/${owner}/${repo}/issues/${prNumber}/comments`, {
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
+      });
+
+      // Delete the comments made by the GitHub Actions bot
+      for (const comment of response.data) {
+        if (comment.user.type === 'Bot' && comment.user.login === 'github-actions[bot]') {
+          await axios.delete(`https://api.github.com/repos/${owner}/${repo}/issues/comments/${comment.id}`, {
+            headers: {
+              Authorization: `Bearer ${authToken}`,
+            },
+          });
+        }
+      }
+    } else {
+      console.log('Not a pull request event. Skipping comment removal.');
+    }
+  } catch (error) {
+    console.error('Error removing comments:', error);
+    throw error;
+  }
+}
+
+removeEvents();
